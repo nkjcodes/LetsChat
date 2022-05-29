91dadd210e5745bf4d597c9c298da2dd71d6f00b
nkjcodes <nirmalkumarjangir.18cs@saividya.ac.in>
2022-05-29 18:28:17 +0530

    Update APIService.java

--- app/src/main/java/com/example/letschat/APIService.java
+++ app/src/main/java/com/example/letschat/APIService.java
@@ -9,7 +9,7 @@ public interface APIService {
     @Headers(
             {
                     "Content-Type:application/json",
-                    "Authorization:key=AAAA5Ufr_D0:APA91bEx6hkucFZYBhyZM7NclgdmsE8thiWdXojVuCx9iy9GV6YMn5zWAgy8dWDiEpvxZP6ULPRK2YTR--N1hM1rG7gL2LG4Zmarjmea6dNixyjy-IHmJ-8ptM14pC5umX2C7xVXKpVU"
+                    "Authorization:key=AAAA5Ufr_D0:APA91bEx6hkucFZYBhyZM7NclgdmsE8thiWdXojVuCx9iy9GV6YMn5zWAgy8dWDiEpvxZP6ULPRK2YTR--N1hM1rG7hdkuydr76r97ruotdo7do7khgKpVU"
             }
     )
 
